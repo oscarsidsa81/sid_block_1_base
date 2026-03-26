f0542981a36765edd3a21baa9b1d4a2a5be6ca05
oscarsidsa81 <ovilches@sid-sa.com>
2026-03-26 16:55:09 +0100

    'depends': [         'sid_stock_cfg',         'sid_product_base',         'sid_product_legacy',     ],

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'sid_block_1_base',
+    'summary': 'Metamodule sid_block_1_base',
+    'version': '15.0.1.0.0',
+    'category': 'Hidden',
+    'author': 'SIDSA',
+    'license': 'LGPL-3',
+    'depends': ['sid_stock_cfg', 'sid_product_base', 'sid_product_legacy'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
